069d4824cea99cc4729768466b4b885a3a061fad
Florian Büther <fbuether@jasminefields.net>
2020-09-20 09:42:53 +0200

    Groups of enemies, called squadrons!

---
+++ src/actors/squadron.ts
@@ -0,0 +1,28 @@
+import * as ex from "excalibur";
+
+import {Enemy} from "../actors/enemy";
+
+
+export class Squadron extends ex.Actor {
+
+  public onInitialize(engine: ex.Engine) {
+    this.body.collider.type = ex.CollisionType.PreventCollision;
+
+
+    // starting line.
+    let y = -(engine.screen.viewport.height / 2) + 100;
+
+    const distance = 60;
+    let count = 11;
+
+    let start = - (distance * count / 2) + (distance / 2);
+
+    for (let i = 0; i < count; i++) {
+      let x = start + distance * i;
+
+      let enemy = new Enemy();
+      enemy.pos = new ex.Vector(x, y);
+      this.scene.add(enemy);
+    }
+  }
+}
--- src/scenes/ingame.ts
+++ src/scenes/ingame.ts
@@ -2,26 +2,23 @@ import * as ex from 'excalibur';
 
 import {Player} from "../actors/player";
 import {Starfield} from "../actors/starfield";
-import {Enemy} from "../actors/enemy";
+import {Squadron} from "../actors/squadron";
 
 
 export class Ingame extends ex.Scene {
   private static readonly kickoffDuration: number = 3; // 3000;
 
   public onInitialize(engine: ex.Engine) {
-
     this.add(new Starfield());
 
     let playerY = engine.screen.viewport.height / 2 - 100;
-    let enemyY = -(engine.screen.viewport.height / 2) + 100;
 
     let player = new Player();
     player.pos = new ex.Vector(0, playerY);
     this.add(player);
 
-    let enemy = new Enemy();
-    enemy.pos = new ex.Vector(0, enemyY);
-    this.add(enemy);
+    let squadron = new Squadron();
+    this.add(squadron);
 
 
     this.camera.zoom(0.1);

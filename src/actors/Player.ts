56e3f2c0bcd45fbc08f9a0e7d753b0f8f5b6cfe1
Florian Büther <fbuether@jasminefields.net>
2020-10-10 17:28:32 +0200

    Player.

---
+++ src/actors/Player.ts
@@ -0,0 +1,76 @@
+
+import {Actor} from "../engine/Actor";
+import {Sprite} from "../engine/components/Sprite";
+import {Engine} from "../engine/Engine";
+import {Loader} from "../engine/Loader";
+import {Body} from "../engine/components/Body";
+
+import {Vector} from "../engine/Vector";
+import {ShapeGenerator} from "../engine/ShapeGenerator";
+
+
+let spriteTex = Loader.add(require("../../assets/images/3rd/player.png"));
+
+
+export class Player extends Actor {
+  private static readonly speed: number = 2400;
+  private static readonly firingSpeed: number = 1000; // 1/s
+
+  private health: number = 5;
+  private lastShot: number = -1;
+
+  private engine: Engine;
+  private body: Body;
+
+
+  public constructor(engine: Engine) {
+    super();
+    this.engine = engine;
+
+    let sprite = new Sprite(engine, spriteTex);
+    this.add(sprite);
+
+    this.body = new Body(engine,
+      new ShapeGenerator().generateFromTexture(sprite.texture));
+    this.body.position = new Vector(20, 20);
+    this.add(this.body);
+
+    sprite.attachTo(this.body);
+  }
+
+
+  public moveTo(position: Vector) {
+    this.body.position = position;
+  }
+
+
+  public update(delta: number) {
+    let movement = Player.speed * delta / 1000;
+
+    let moveRight = this.engine.keyboard.isPressed("d");
+    let moveLeft = this.engine.keyboard.isPressed("a");
+    if (moveRight && !moveLeft) {
+      this.body.moveBy(new Vector(movement, 0));
+    }
+    else if (moveLeft && !moveRight) {
+      this.body.moveBy(new Vector(-movement, 0));
+    }
+
+    // firing.
+
+    if (this.lastShot > 0) {
+      this.lastShot -= delta;
+    }
+
+    let fires = this.engine.keyboard.isPressed(" ");
+    if (fires && this.lastShot <= 0) {
+      // new bullet!
+      this.lastShot = Player.firingSpeed;
+    }
+  }
+
+
+  public getCharge() {
+    return Math.min(1, Math.max(0, 1 - (this.lastShot / 1000)));
+  }
+}

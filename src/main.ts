85b1b2212d4bd4378c8d9fcaf3ac94f77c3b1b59
Florian Büther <fbuether@jasminefields.net>
2020-11-08 09:23:19 +0100

    Sounds! All the sounds!

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@
 import {Engine} from "./engine/Engine";
 import {MainMenu} from "./scenes/MainMenu";
 
+// pixi-sound must be linked here already, otherwise some strange kind of
+// race condition causes sounds to no be loaded properly. Who knows.
+import "pixi-sound";
+
 
 let engine = Engine.create();
 engine.start(MainMenu);
